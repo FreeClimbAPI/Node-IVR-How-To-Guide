b585bcd22b6cdf713712f2e825763878e85786e2
Matt Rohan <mrohan@vailsys.com>
2021-03-04 10:33:10 -0600

    re-add correct unit tests for cr2.0

--- call-router-2.0/index.js
+++ call-router-2.0/index.js
@@ -2,6 +2,7 @@ require('dotenv-safe').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 const freeclimbSDK = require('@freeclimb/sdk')
 
