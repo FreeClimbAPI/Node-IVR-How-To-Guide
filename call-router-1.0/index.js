9fb90df81743bfc2e8365b94008a53704feb1aec
Matt Rohan <mrohan@vailsys.com>
2021-03-15 12:06:57 -0500

    add correct bodyparser.use line

--- call-router-1.0/index.js
+++ call-router-1.0/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 const freeclimbSDK = require('@freeclimb/sdk')
 
 const port = process.env.PORT || 3000
